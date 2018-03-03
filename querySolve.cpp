614ea2064d5ed9dfec4f0f5e5626ff687d20096e
Saurbhi <saurbhiagarwal1595@gmail.com>
2018-03-03 19:17:23 +0530

    Solved query problem with Kadu

--- querySolve.cpp
+++ querySolve.cpp
@@ -1,24 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+typedef long long ll;
+ll N;
+
+void solveQueryOne(ll index, ll value, ll lastAnswer, vector<vector<ll> > &seqList) {
+	ll seq;
+	seq = (index ^ lastAnswer) % N;
+	seqList[seq].push_back(value);
+}
+
+ll solveQueryTwo(ll index, ll value, ll lastAnswer, vector<vector<ll> > seqList) {
+	ll seq;
+	seq = (index ^ lastAnswer) % N;
+	return seqList[seq][value % seqList[seq].size()];
+}
+
 int main() {
 
-int N, Q, lastAnswer = 0;
-int queryNo, index, value;
+ll lastAnswer = 0;
+ll Q, queryNo;
+ll index, value;
 cin >> N >> Q;
 
-list<list<int>> seqList(N);
+vector<vector<ll> > seqList(N);
 
-for(int i = 0; i < Q; i++) {
+for(ll i = 0; i < Q; i++) {
 	cin >> queryNo >> index >> value;
 	
-	if (queryNo == 1)
-		solveQueryOne();
-	else
-		solveQueryTwo();
+	if (queryNo == 1) {
+		solveQueryOne(index, value, lastAnswer, seqList);
+		
+	}	
+	else {
+		lastAnswer = solveQueryTwo(index, value, lastAnswer, seqList);
+		cout << lastAnswer <<endl;
+	}
 }
 
 return 0;
 }
-
-	
